59b3a3949f8bdd3fece2fe83c7b293e3c961119a
KaterynaValuiska <katya.barry.ubf@gmail.com>
2023-06-12 15:07:29 +0200

    fetch done

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const input = document.querySelector('[name="searchQuery"]');
 const btnSubmit = document.querySelector('[type="submit"]');
+const gallery = document.querySelector('.gallery');
 
 
 input.addEventListener('change', onInput);
@@ -11,40 +12,65 @@ let inputValue = '';
 
 function onInput(evt) {
     inputValue = evt.target.value;
-    console.log(inputValue);
-  fetchRequest(inputValue)
 }
 
 function onSubmit(evt) {
     evt.preventDefault();
-    console.log(inputValue); 
+  fetchRequest(inputValue)
+    .then((cards) => {
+      console.log(cards);
+      renderCards(cards); 
+      
+   })
+    .catch((error) => console.log(error));
 }
 
 
 const API_KEY = '37154434-e108fb93a0dd643270de780f1';
-const baseURL = 'https://pixabay.com/api/';
-
-// const instance = axios.create({
-//     baseURL: 'https://pixabay.com/api/', 
-//   headers: {
-//     'x-api-key': API_KEY,
-//   },
-// });
 
 async function fetchRequest(inputValue) {
     try {
       const response = await axios.get('https://pixabay.com/api/', {
         params: {
-          API_KEY: API_KEY,
+          key: API_KEY,
           q: inputValue,
           image_type: "photo",
           orientation: "horizontal",
           safesearch: true,
+          per_page: 40,
         }
       });
    console.log(response);
   } catch (error) {
     console.error(error);
   }
+}
+
+function renderCards(cards) {
+  const markup = cards
+    .map(({tags, webformatURL}) => {
+      return `
+      <div class="photo-card">
+  <img src="${webformatURL}" alt=${tags} loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b>
+    </p>
+    <p class="info-item">
+      <b>Views</b>
+    </p>
+    <p class="info-item">
+      <b>Comments</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads</b>
+    </p>
+  </div>
+</div>
+      `;
+    })
+    .join("");
+  
+  gallery.innerHTML = markup;
 }
 
