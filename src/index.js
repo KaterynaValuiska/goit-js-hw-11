f2bd5eda77cbba1a1803d651af6d393e61836ced
KaterynaValuiska <katya.barry.ubf@gmail.com>
2023-06-12 22:20:56 +0200

    styles, load more

--- src/index.js
+++ src/index.js
@@ -1,33 +1,62 @@
 import axios from 'axios';
+import Notiflix from 'notiflix';
 
-const input = document.querySelector('[name="searchQuery"]');
-const btnSubmit = document.querySelector('[type="submit"]');
+const searchForm = document.querySelector('.search-form');
 const gallery = document.querySelector('.gallery');
-
-
-input.addEventListener('change', onInput);
-btnSubmit.addEventListener('click', onSubmit);
-
-let inputValue = '';
-
-function onInput(evt) {
-    inputValue = evt.target.value;
-}
-
-function onSubmit(evt) {
-  evt.preventDefault();
-  
-  fetchRequest(inputValue)
-    .then((data) => gallery.insertAdjacentHTML('beforeend', renderCards(data.hits)))
-    .catch((error) => console.log(error));
-}
-
+const loadMore = document.querySelector('.load-more');
 
 const API_KEY = '37154434-e108fb93a0dd643270de780f1';
 
-async function fetchRequest(inputValue) {
+searchForm.addEventListener('submit', onSubmit);
+loadMore.addEventListener('click', onLoadMore)
+
+
+let inputValue = '';
+let currentPage = 1;
+
+async function onSubmit(evt) {
+
+  evt.preventDefault();
+
+  gallery.innerHTML = '';
+  inputValue = evt.currentTarget.elements.searchQuery.value;
+  try {
+
+    const { hits, totalHits } = await fetchRequest(inputValue, page=1);
+    if (hits.length === 0) {
+      gallery.innerHTML = '';
+      loadMore.hidden = true;
+      errorShow();
+      return;
+    }
+    console.log(hits);
+    gallery.insertAdjacentHTML('beforeend', renderCards(hits));
+    if (totalHits !== page) {
+      loadMore.hidden = false;
+    }
+  } catch (error) {
+    console.error(error);
+    errorShow()
+  } 
+    
+}
+
+async function onLoadMore() {
+  currentPage += 1;
+  try {
+    const { hits } = await fetchRequest(inputValue, currentPage);
+    console.log(hits);
+    gallery.insertAdjacentHTML('beforeend', renderCards(hits));
+    loadMore.classList.remove('hide');
+  } catch (error) {
+    console.error(error);
+    errorShow()
+  } 
+}
+
+async function fetchRequest(inputValue, page=1) {
     try {
-      const response = await axios.get('https://pixabay.com/api/', {
+      const {data} = await axios.get('https://pixabay.com/api/', {
         params: {
           key: API_KEY,
           q: inputValue,
@@ -35,38 +64,44 @@ async function fetchRequest(inputValue) {
           orientation: "horizontal",
           safesearch: true,
           per_page: 40,
+          page: page,
         }
       });
-   console.log(response);
+      return data;
    
   } catch (error) {
-    console.error(error);
+      console.error(error);
+      errorShow()
   }
 }
 
 function renderCards(cards) {
   return cards
-    .map(({tags, webformatURL}) => {
+    .map(({tags, webformatURL, likes, views, comments, downloads}) => {
       return `
       <div class="photo-card">
-  <img src="${webformatURL}" alt=${tags} loading="lazy" />
+  <img src="${webformatURL}" alt=${tags} loading="lazy" class="photo"/>
   <div class="info">
     <p class="info-item">
-      <b>Likes</b>
+      <b>Likes: ${likes} </b>
     </p>
     <p class="info-item">
-      <b>Views</b>
+      <b>Views: ${views}</b>
     </p>
     <p class="info-item">
-      <b>Comments</b>
+      <b>Comments: ${comments}</b>
     </p>
     <p class="info-item">
-      <b>Downloads</b>
+      <b>Downloads: ${downloads}</b>
     </p>
   </div>
 </div>
       `;
     })
     .join("");
+}
+
+function errorShow() {
+  Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
 }
 
