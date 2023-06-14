eb68def27e4281872a9377cbb31eb7dfdc3fee60
KaterynaValuiska <katya.barry.ubf@gmail.com>
2023-06-14 13:51:29 +0200

    api-render

---
+++ src/api-render.js
@@ -0,0 +1,64 @@
+import axios from 'axios';
+
+const API_KEY = '37154434-e108fb93a0dd643270de780f1';
+const perPage = 40;
+
+
+async function fetchRequest(inputValue, currentPage) {
+    try {
+      const {data} = await axios.get('https://pixabay.com/api/', {
+        params: {
+          key: API_KEY,
+          q: inputValue,
+          image_type: "photo",
+          orientation: "horizontal",
+          safesearch: true,
+          per_page: perPage,
+          page: currentPage,
+        }
+      });
+      return data;
+   
+  } catch (error) {
+      console.error(error);
+      errorShow()
+  }
+}
+
+function renderCards(cards) {
+  return cards
+    .map(({largeImageURL, tags, webformatURL, likes, views, comments, downloads}) => {
+      return `
+      <div class="photo-card">
+ <a href=${largeImageURL}>
+  <img src="${webformatURL}" alt= "${tags}" loading="lazy" class="photo"/>
+  </a>
+  <div class="info">
+    <p class="info-item">
+      <b>Likes:  </b>
+      <span>${likes} </span>
+    </p>
+    <p class="info-item">
+      <b>Views: </b>
+      <span>${views}</span>
+    </p>
+    <p class="info-item">
+      <b>Comments: </b>
+      <span>${comments}</span>
+    </p>
+    <p class="info-item">
+      <b>Downloads: </b>
+      <span>${downloads}</span>
+    </p>
+  </div>
+</div>
+      `;
+    })
+    .join("");
+}
+
+function errorShow() {
+  Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+}
+
+export { errorShow, renderCards, fetchRequest, perPage };
--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-import axios from 'axios';
+import { errorShow, renderCards, fetchRequest, perPage  } from "./api-render";
 import Notiflix from 'notiflix';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
+
 
 
 const searchForm = document.querySelector('.search-form');
 const gallery = document.querySelector('.gallery');
 const loadMore = document.querySelector('.load-more');
 
-const API_KEY = '37154434-e108fb93a0dd643270de780f1';
 
 searchForm.addEventListener('submit', onSubmit);
 loadMore.addEventListener('click', onLoadMore)
 
-const perPage = 40;
+
 let inputValue = '';
 let currentPage = 1;
 
@@ -29,6 +29,7 @@ async function onSubmit(evt) {
 
   gallery.innerHTML = '';
   inputValue = evt.currentTarget.elements.searchQuery.value;
+  currentPage = 1;
   try {
     const { hits, totalHits } = await fetchRequest(inputValue, currentPage);
     if (hits.length === 0) {
@@ -88,60 +89,5 @@ window.scrollBy({
   } 
 }
 
-async function fetchRequest(inputValue, currentPage) {
-    try {
-      const {data} = await axios.get('https://pixabay.com/api/', {
-        params: {
-          key: API_KEY,
-          q: inputValue,
-          image_type: "photo",
-          orientation: "horizontal",
-          safesearch: true,
-          per_page: perPage,
-          page: currentPage,
-        }
-      });
-      return data;
-   
-  } catch (error) {
-      console.error(error);
-      errorShow()
-  }
-}
 
-function renderCards(cards) {
-  return cards
-    .map(({largeImageURL, tags, webformatURL, likes, views, comments, downloads}) => {
-      return `
-      <div class="photo-card">
- <a href=${largeImageURL}>
-  <img src="${webformatURL}" alt= "${tags}" loading="lazy" class="photo"/>
-  </a>
-  <div class="info">
-    <p class="info-item">
-      <b>Likes:  </b>
-      <span>${likes} </span>
-    </p>
-    <p class="info-item">
-      <b>Views: </b>
-      <span>${views}</span>
-    </p>
-    <p class="info-item">
-      <b>Comments: </b>
-      <span>${comments}</span>
-    </p>
-    <p class="info-item">
-      <b>Downloads: </b>
-      <span>${downloads}</span>
-    </p>
-  </div>
-</div>
-      `;
-    })
-    .join("");
-}
-
-function errorShow() {
-  Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
-}
 
